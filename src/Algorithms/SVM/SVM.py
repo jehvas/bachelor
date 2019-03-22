ba3b73682a6e1133404af5926c8aee0df32dd0aa
Mads <madpe@itu.dk>
2019-03-22 11:38:17 +0100

    confusmatrixos

--- src/Algorithms/SVM/SVM.py
+++ src/Algorithms/SVM/SVM.py
@@ -1,28 +1,25 @@
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.model_selection import train_test_split as tts
-from sklearn.svm import SVC, LinearSVC
-from sklearn.model_selection import train_test_split as tts, StratifiedKFold
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
 
-from DatasetsConsumers.Chromium import Chromium
+from DatasetsConsumers.Newsgroups import Newsgroups
 from Glove.glovemodel import GloVe
 
 print("Running algorithm: Algorithms.SVM")
 
 # Load dataset
-Dataset_Consumer = Chromium()
+Dataset_Consumer = Newsgroups()
 emails, labels = Dataset_Consumer.load(True)
 
 # Load GloVe model
-GloVe_Obj = GloVe("glove.6B.50d.txt", Dataset_Consumer)
-features = GloVe_Obj.get_features(emails)
+GloVe_Obj = GloVe(50)
+features = GloVe_Obj.get_features(emails, Dataset_Consumer)
 
 # Create training data
 x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2, random_state=1)
 
 # Algorithms.SVM Stuff
 svm_classifier = LinearSVC()
-
 
 print("\nStarting fitting")
 svm_classifier.fit(x_train, y_train)
