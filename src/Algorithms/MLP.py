58300e143fedfbd49f6da94407070d8a1d469c8b
Mads <madpe@itu.dk>
2019-03-25 12:32:56 +0100

    precision, recall, f_score calc in MLP

--- src/Algorithms/MLP.py
+++ src/Algorithms/MLP.py
@@ -1,9 +1,9 @@
-from collections import Counter
+import numpy as np
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data
+from sklearn.metrics import f1_score, precision_recall_fscore_support
 from sklearn.model_selection import train_test_split as tts
 from torch import optim
 
@@ -17,9 +17,8 @@ if __name__ == '__main__':
     emails, labels = Dataset_Consumer.load(True)
 
     # Load GloVe model
-    GloVe_Obj = GloVe(100)
+    GloVe_Obj = GloVe(50)
     features = GloVe_Obj.get_features(emails, Dataset_Consumer)
-
 
     # Create training data & SVM Stuff
     x_train, x_test, y_train, y_test = tts(features, labels, test_size=0.2, random_state=1)
@@ -46,13 +45,13 @@ if __name__ == '__main__':
                 super(Net, self).__init__()
 
                 self.relu = nn.ReLU()
-                #self.embedding = nn.Embedding(len(Dataset_Consumer.vocabulary), GloVe_Obj.dimensionCount)
+                # self.embedding = nn.Embedding(len(Dataset_Consumer.vocabulary), GloVe_Obj.dimensionCount)
                 self.fc1 = nn.Linear(input_size, hidden_size)
                 self.fc2 = nn.Linear(hidden_size, hidden_size)
                 self.fc3 = nn.Linear(hidden_size, output_size)
 
             def forward(self, x):
-                #x = self.embedding(x)
+                # x = self.embedding(x)
                 x = self.fc1(x)
                 x = self.relu(x)
                 x = self.fc2(x)
@@ -65,6 +64,8 @@ if __name__ == '__main__':
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9)
 
+        predictions_total = []
+
         for epoch in range(num_epochs):  # loop over the dataset multiple times
             print("Epoch: ", epoch + 1, "/", num_epochs)
             running_loss = 0.0
@@ -73,7 +74,6 @@ if __name__ == '__main__':
                 # get the inputs
                 inputs, labels = data
                 inputs = inputs.float()
-
                 optimizer.zero_grad()
                 outputs = net(inputs)
 
@@ -88,48 +88,23 @@ if __name__ == '__main__':
                           (epoch + 1, i + 1, running_loss / 200))
                     running_loss = 0.0'''
 
-        #print('Finished Training')
+        # print('Finished Training')
 
-        correct = 0
-        total = 0
         for i, data in enumerate(testloader, 0):
             net.zero_grad()
             inputs, labels = data
             outputs = net(inputs.float())
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
 
-        '''with torch.no_grad():
-            for data in testloader:
-                inputs, labels = data
-                outputs = net(inputs.float())
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()'''
+            predictions_total = np.concatenate([predictions_total, predicted.numpy()])
 
-        print("nodes: ", hidden_size, ': Accuracy: %d %%' % (
-                100 * correct / total))
+        precision, recall, fbeta_score, support = precision_recall_fscore_support(y_test, predictions_total,
+                                                                                  average="micro")
+        print("\n--- Results ---")
+        print("Precision: ", precision)
+        print("\n\nRecall: ", recall)
+        print("\n\nF_score: ", fbeta_score)
 
 
     for i in range(1, 2):
-        run(100, 40)
-
-    '''
-    # MLP Stuff
-    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(500), random_state=1)
-    
-    print("\nStarting fitting")
-    clf.fit(x_train, y_train)
-    
-    print("Fitting done")
-    predictions = clf.predict(x_test)
-    
-    precision, recall, fbeta_score, support = precision_recall_fscore_support(y_test, predictions)
-    print("\n--- Results ---")
-    print("Precision: ", precision)
-    print()
-    print("\n\nRecall: ", recall)
-    print("\n\nF_score: ", fbeta_score)
-    
-    '''
+        run(100, 25)
