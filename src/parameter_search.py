a7c1f560e8a9101b91358113e8ac713cc2d6b5ba
Jens Hvass <jehj@itu.dk>
2019-04-11 14:35:28 +0200

    Parameter search

--- src/parameter_search.py
+++ src/parameter_search.py
@@ -1,20 +1,32 @@
 import time
 
 import datetime
+
+import numpy as np
 from sklearn.metrics import precision_recall_fscore_support
 
 from Algorithms.MLPT import MLP_tensorflow
 from DatasetsConsumers.Newsgroups import Newsgroups
 # Load dataset
 from Glove.glovemodel import GloVe
+from rootfile import ROOTPATH
 from utility.Parameters import get_params
 
 
-def log_to_file(text):
-    log_string = "{} {}".format(datetime.datetime.now(), text)
-    print(log_string)
+def log_to_file(idx, parameters, precision, recall, fscore):
+    avg = sum(fscore) / len(fscore)
+    log_string = "{}\t{}\t{}\t{}\t{}\t{}\t{},".format(idx,
+                                                      avg,
+                                                      parameters,
+                                                      np.array2string(precision, separator=';', max_line_width=500),
+                                                      np.array2string(recall, separator=';', max_line_width=500),
+                                                      np.array2string(fscore, separator=';', max_line_width=500),
+                                                      datetime.datetime.now())
+    with open(ROOTPATH + 'Results/resultsfile.csv', 'a+') as f:
+        f.write(log_string + '\n')
 
 
+counter = 0
 dataset_consumer = Newsgroups()
 algorithm = MLP_tensorflow
 
@@ -35,8 +47,7 @@ while True:
         'output_dim': len(set(labels)),
         'input_dim': features.shape[1]
     }
-
-    log_to_file(str(parameters))
+    print("\n#### STARTING RUN NUMBER {} #####\n".format(counter))
 
     data_to_plot, y_test, rounded_predictions = algorithm.run_train(dataset_consumer, features, labels,
                                                                     parameters, None, None,
@@ -48,3 +59,6 @@ while True:
     print("\nFscore: ", fscore)
     print("\n")
     print("Avg fScore:", (sum(fscore) / len(fscore)))
+
+    log_to_file(counter, str(parameters), precision, recall, fscore)
+    counter += 1
