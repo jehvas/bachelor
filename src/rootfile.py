98da0571dcd8d2156f2cdd3da550219e520fe9a9
Jens Hvass <jehj@itu.dk>
2019-04-11 14:08:03 +0200

    Rootfile igen

--- src/rootfile.py
+++ src/rootfile.py
@@ -2,3 +2,8 @@ import os
 
 ROOTPATH = os.path.dirname(__file__) + '/../'
 
+if os.path.isdir(ROOTPATH + 'out/'):
+    print('Running locally. Using standard ROOTPATH')
+else:
+    ROOTPATH = os.path.dirname(__file__) + '/../../drive/My Drive/Bachelor'
+    print('Running on COLAB. Using alertnate ROOTPATH')
