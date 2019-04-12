cac4b07434ca1b648196a1501efd09fc2ba4a147
Mads <madpe@itu.dk>
2019-04-12 12:25:18 +0200

    Sensitive Enron. Slettet devconsumers

---
+++ src/utility/ai_util.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  7 08:16:27 2017
+@author: neerbek
+"""
+import time
+import ast
+import operator as op
+import zipfile
+from numpy.random import RandomState  # type: ignore
+from typing import List
+
+
+class Timer:
+    def __init__(self, name="Timer"):
+        self.elapsed = 0
+        self.start = None
+        self.count = 0
+        self.name = name
+
+    def begin(self):  # function cannot be named start
+        self.start = time.time()
+
+    def end(self, count=1):
+        if self.start != None:
+            end = time.time()
+            self.elapsed += end - self.start
+            self.count += count
+            self.start = None
+        else:
+            print(
+                "{} end() called, but timer is not started".format(self.name))
+        return self
+
+    def ratio(self):
+        "seconds per count"
+        if self.count == 0:
+            return 0
+        return (self.elapsed + 0.0) / self.count
+
+    def report(self, count=None):
+        if count != None:
+            self.count = count
+        print(
+            "{}: Number of updates: {}. Total time: {:.2f}. Average time: {:.4f} sec".
+                format(self.name, self.count, self.elapsed, self.ratio()))
+
+
+class TimerList:
+    def __init__(self):
+        self.timers: List[Timer];
+        self.timers = []
+        self.lastReport = time.time()
+        self.totalTimer = None  # type:Timer
+        self.forwardTimer = None  # type:Timer
+        self.backwardTimer = None  # type:Timer
+
+    def addTimer(self, timer):
+        self.timers.append(timer)
+
+    def end(self):
+        for t in self.timers:
+            if t.start != None:
+                t.end()
+
+    def update(self):
+        end = time.time()
+        for t in self.timers:
+            if t.start != None:
+                t.elapsed += end - t.start
+                t.start = end
+
+    def do_report(self, min_seconds=-1, t=time.time()):
+        if (t - self.lastReport < min_seconds):
+            return False
+        return True
+
+    def report(self, min_seconds=-1, update_timers=False):
+        t = time.time()
+        if not self.do_report(min_seconds, t):
+            return False
+        if update_timers:
+            self.update()
+        self.lastReport = t
+        for timer in self.timers:
+            timer.report()
+        return True
+
+
+# evaluate math expressions, from
+# https://stackoverflow.com/questions/2371436/evaluating-a-mathematical-expression-in-a-string
+#
+# supported operators
+operators = {ast.Add: op.add, ast.Sub: op.sub, ast.Mult: op.mul,
+             ast.Div: op.truediv, ast.Pow: op.pow, ast.BitXor: op.xor,
+             ast.USub: op.neg}
+
+
+def eval_expr(expr):
+    """
+    >>> eval_expr('2^6')
+    4
+    >>> eval_expr('2**6')
+    64
+    >>> eval_expr('1 + 2*3**(4^5) / (6 + -7)')
+    -5.0
+    """
+    return eval_(ast.parse(expr, mode='eval').body)
+
+
+def eval_(node):
+    if isinstance(node, ast.Num):  # <number>
+        return node.n
+    elif isinstance(node, ast.BinOp):  # <left> <operator> <right>
+        return operators[type(node.op)](eval_(node.left), eval_(node.right))  # type: ignore
+    elif isinstance(node, ast.UnaryOp):  # <operator> <operand> e.g., -1
+        return operators[type(node.op)](eval_(node.operand))  # type: ignore
+    else:
+        raise TypeError(node)
+
+
+class AIFileWrapper():
+    def __init__(self, filename):
+        self.filename = filename
+        self.myzip = None
+        self.fd = None
+
+    def __enter__(self):
+        index = self.filename.find("$")
+        if index != -1:  # assume zipfile
+            zipfilename = self.filename[:index]
+            internalfilename = self.filename[index + 1:]
+            self.myzip = zipfile.ZipFile(zipfilename)
+            self.fd = self.myzip.open(internalfilename, mode='r')  # always binary, even with mode='r' [sic]
+        else:
+            self.fd = open(self.filename, 'rb')  # io.open does not support binary it seems
+        return self
+
+    def __exit__(self, type, value, traceback):
+        # Exception handling here
+        if self.fd != None:
+            self.fd.close()
+        if self.myzip != None:
+            self.myzip.close()
+
+    def toStrippedString(self, line):
+        # print(line)
+        line = str(line, encoding="utf8")
+        if line.endswith("\r\n"):
+            line = line[:-2]
+        if line.endswith("\n"):
+            line = line[:-1]
+        # Strips newline. Consider:
+        # http://stackoverflow.com/questions/509446/python-reading-lines-w-o-n
+        return line
+
+
+def shuffleList(a, rng=RandomState(1234)):
+    """Expects a to be a list type. Shuffles all elements and return new list"""
+    perm = rng.permutation(len(a))  # we have seen issues using the built-in shuffle
+    aNew = [a[i] for i in perm]
+    return aNew
---
+++ src/utility/load_trees.py
@@ -0,0 +1,472 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  1 12:36:32 2016
+@author: neerbek
+"""
+import io
+from utility import ai_util
+
+DEBUG_PRINT = False
+
+class Warnings:
+    warnRootNodeWord = 0
+
+
+class Node:
+    def __init__(self, parent=None):
+        self.parent = parent
+        self.word = None
+        self.syntax = None
+        self.representation = None
+        self.left = None
+        self.right = None
+        self.label = None
+        self.update_count = -1
+
+    def clone(self, node):
+        # update clone_tree also
+        self.parent = node.parent
+        self.word = node.word
+        self.syntax = node.syntax
+        self.representation = node.representation
+        self.left = node.left
+        self.right = node.right
+        self.label = node.label
+        self.update_count = node.update_count
+
+    def add_child(self):
+        if (self.left == None):
+            self.left = Node(self)
+            return self.left
+        elif self.right == None:
+            self.right = Node(self)
+            return self.right
+        raise Exception("max two children per Node")
+
+    def number_of_children(self):
+        count = 0
+        if self.left != None:
+            count += 1
+        if self.right != None:
+            count += 1
+        return count
+
+    def is_leaf(self):
+        return (self.number_of_children() == 0)
+
+    def replace_nodenames(self, newname):
+        self.syntax = newname
+        if self.left != None:
+            self.left.replace_nodenames(newname)
+        if self.right != None:
+            self.right.replace_nodenames(newname)
+
+    def simplify(self):
+        # removes nodes which only have 1 child
+        if self.left != None:
+            self.left.simplify()
+        if self.right != None:
+            self.right.simplify()
+
+        if self.number_of_children() != 1:
+            return
+
+        if self.word != None:
+            raise Exception(
+                "cannot simplify tree with words in intermidiate nodes")
+
+        if self.left != None:
+            self.clone(self.left)
+        elif self.right != None:
+            self.clone(self.right)
+
+    def is_binary(self):
+        if self.number_of_children() == 0:
+            return True
+        if self.number_of_children() == 1:
+            return False
+        if self.number_of_children() == 2:
+            return (self.left.is_binary() and self.right.is_binary())
+        return False  # more than 2 children
+
+    def has_only_words_at_leafs(self):
+        if self.number_of_children() == 0:
+            return (
+                self.word != None
+            )  # a self.word==None for leaf nodes is also a reason for reporting false
+        if self.word != None:
+            if self.parent != None:  # workaround for "hack" for monsanto
+                return False
+        return (self.left.has_only_words_at_leafs() and
+                self.right.has_only_words_at_leafs())
+
+
+def clone_tree(node, parent=None):
+    if node == None:
+        return None
+
+    n = Node()
+    n.parent = parent
+    n.word = node.word  # expected immutable string or int
+    n.syntax = node.syntax  # expected immutable string or int
+    n.representation = node.representation  # if this is an array we might need to copy this
+    n.left = clone_tree(node.left, n)
+    n.right = clone_tree(node.right, n)
+    n.update_count = node.update_count  # expected immutable string or int
+    return n
+
+
+def as_array(node):
+    if node == None:
+        return []
+    res = [node]
+    res.extend(as_array(node.left))
+    res.extend(as_array(node.right))
+    return res
+
+
+def read_rep(l, index, node):
+    """ read l from index and until ] is encountered, return floats read as a rep array"""
+    i = index
+    rep = []
+    word = ""
+    while i < len(l):
+        c = l[i]
+        i += 1
+        if (c == ' ' or c == ']'):
+            if len(word) > 0:
+                f = float(word)  # may throw
+                rep.append(f)
+                word = ""
+            if c == ']':
+                break
+        else:
+            word += c
+    node.representation = rep
+    return i
+
+
+def output(node):
+    """ outputs a string with the textual representation of the node and it's children"""
+    if (node == None):
+        return ""
+    if node.syntax is None:
+        o = " (" + "TabooNone"
+        print("Warning: outputting TabooNone")
+    else:
+        o = " (" + node.syntax
+    if (node.word != None):
+        o += " " + node.word
+    return o + output(node.left) + output(node.right) + ")"
+
+
+def output_sentence_impl(node):
+    """ outputs the words in the node and it's children"""
+    if (node == None):
+        return ""
+    o = output_sentence(node.left)
+    w = node.word
+    if (w != None):
+        if w == '.' or w == ',' or w == ':' or w == ';':
+            o += w
+        else:
+            o += " " + w
+    o += output_sentence(node.right)
+    return o
+
+
+def output_sentence(node):
+    """ outputs the words in the node and it's children"""
+    if (node == None):
+        return ""
+    o = output_sentence_impl(node)
+    # o = unescape_sentence(o)
+    # for the python parser it works best if we use -LRB- and -RRB- (i.e. escaped sentences)
+    return o
+
+
+def output_lenrep(node):
+    if (node == None):
+        return ""
+    o = " (" + node.syntax
+    o += ":{}".format(len(node.representation))
+    if (node.word != None):
+        o += " " + node.word
+    return o + output_lenrep(node.left) + output_lenrep(node.right) + ")"
+
+
+def parse_line(l, index, node):
+    i = index
+    # read node name ("syntaxname")
+    syntaxname = ""
+    while i < len(l):
+        c = l[i]
+        i += 1
+        if (c == ' '):
+            break
+        syntaxname += c
+    if DEBUG_PRINT and len(syntaxname) == 0:
+        print("found node with no syntax name. Assuming leaf node.")
+    # sep =syntaxname.find(":")
+    #    if sep!=-1:
+    #        node.syntax = syntaxname[:sep]
+    #        node.word = syntaxname[sep+1:]
+    #    else:
+    node.syntax = syntaxname
+    # read node info (incl. children)
+    word = ""
+    while i < len(l):
+        c = l[i]
+        i += 1
+        if c == '(':
+            if len(word) != 0:
+                raise Exception(
+                    "Found begin parenthese while parsing word {}:\"{}\"".
+                    format(i, word))
+            if len(syntaxname) == 0:
+                raise Exception(
+                    "Found begin parenthese while having no syntax name (implying leaf) {}".
+                    format(i))
+            child = node.add_child()
+            i = parse_line(l, i, child)
+            continue
+        elif c == ')':
+            if len(word) != 0:
+                if node.word != None:
+                    raise Exception(
+                        "found word in tree but node already has word {}:\"{}\"".
+                        format(i, word))
+                node.word = word
+                # word = ""
+            else:
+                if len(syntaxname) == 0:
+                    raise Exception(
+                        "Ending node without syntaxname {}".format(i))
+            break
+        elif c == ' ':
+            if len(word) != 0:
+                if node.parent == None:
+                    if Warnings.warnRootNodeWord < 1:
+                        print("WARN: found root node with word encoded. We allow as a workaround")
+                    Warnings.warnRootNodeWord += 1
+                    node.word = word
+                    word = ""
+                else:
+                    raise Exception(
+                        "Found space while parsing word {}:\"{}\"".format(i, word))
+            continue
+        elif c == '[':
+            if len(word) != 0:
+                raise Exception(
+                    "Found square bracket while parsing word {}:\"{}\"".format(
+                        i, word))
+            i = read_rep(l, i, node)
+            continue
+        else:
+            word += c
+    return i
+
+def get_tree(line, fn="get_tree"):
+    if not line.startswith(" ("):
+        raise Exception(fn + " line does not start with \" (\"")
+    tree = Node(None)
+    # print("Line is " + line)
+    i = parse_line(line, 2, tree)
+    if i < len(line) - 1:  # Not all of line parsed
+        raise Exception(
+            fn +
+            " parsing line failed. There was more than one tree in the line. {}".
+            format(i))
+    l2 = output(tree)
+    if l2 != line:  # Lines differ
+        raise Exception(fn + " marshall and unmarshalling differs" +
+                        "\n" + line + "\n" + l2)
+    if not tree.is_binary():
+        raise Exception(fn + " tree is not binary")
+    if not tree.has_only_words_at_leafs():
+        raise Exception(fn + " tree is not properly normalized")
+    return tree
+
+# filename = "trees/train.txt"
+# max_count = -1
+def get_trees(file, max_count=-1):
+    filename = file
+    count = 0
+    trees = []
+    fn = "get_trees"  # function name
+    Warnings.warnRootNodeWord = 0
+    with ai_util.AIFileWrapper(filename) as aifileWrapper:
+        for line in aifileWrapper.fd:
+            line = aifileWrapper.toStrippedString(line)
+            if max_count > -1 and count > max_count:
+                break
+            tree = get_tree(line, fn)
+            if tree.is_leaf():
+                print("tree is one word. Ignoring")
+                continue
+            trees.append(tree)
+            count += 1
+            if count % 2000 == 0:
+                print("Extracted: ", count)
+    print(fn + " done. Count={}. Roots with words count {}".format(count, Warnings.warnRootNodeWord))
+    return trees
+
+def put_trees(filename, trees):
+    count = 0
+    fn = "put_trees "  # function name
+    with io.open(filename, 'w', encoding='utf8') as f:
+        for t in trees:
+            treeString = output(t)
+            f.write(treeString + "\n")
+            count += 1
+    print(fn + "done. Count={}".format(count))
+
+
+def get_fraction(trees, report=False):
+    count_0 = 0
+    count_4 = 0
+    for t in trees:
+        if t.syntax == "0":
+            count_0 += 1
+        elif t.syntax == "4":
+            count_4 += 1
+        else:
+            raise Exception(
+                "unknown syntax encountered in tree: {}".format(t.syntax))
+    res = count_4 / (count_0 + count_4)
+    if report:
+        print("Total count {}, sensitive {} fraction sensitive {:.4f}".format(
+            count_0 + count_4, count_4, res))
+    return res
+
+
+def escape_sentence(l2):
+    "removes parenteses and other stuff from sentence"
+    l2 = l2.replace('(', '-LRB- ').replace(')', ' -RRB-')
+    l2 = l2.replace('[', '-LSB- ').replace(']', ' -RSB-')
+    l2 = l2.replace('{', '-LCB- ').replace('}', ' -RCB-')
+    l2 = l2.replace('&', '-AMP-')
+    return l2
+
+
+def unescape_sentence(l2):
+    "adds right parenteses and other stuff from sentence. Note we can't add correct spacing (user may use spacing in a number of different ways)"
+    l2 = l2.replace('-LRB-', '(').replace('-RRB-', ')')
+    l2 = l2.replace('-LSB-', '[').replace('-RSB-', ']')
+    l2 = l2.replace('-LCB-', '{').replace('-RCB-', '}')
+    l2 = l2.replace('-AMP-', '&')
+    return l2
+
+
+def count_leaf_nodes(node, count=0):
+    if node == None:
+        return count
+    if node.is_leaf():
+        return count + 1
+    count = count_leaf_nodes(node.left, count)
+    count = count_leaf_nodes(node.right, count)
+    return count
+
+def count_non_leaf_nodes(node, count=0):
+    if node == None:
+        return count
+    if node.is_leaf():
+        return count
+    count += 1
+    count = count_non_leaf_nodes(node.left, count)
+    count = count_non_leaf_nodes(node.right, count)
+    return count
+
+def count_nodes(node, count=0):
+    if node == None:
+        return count
+    count += 1
+    count = count_non_leaf_nodes(node.left, count)
+    count = count_non_leaf_nodes(node.right, count)
+    return count
+
+# cleaners
+def cleanTreesByLength(trees, sentenceCutoffLow=5, sentenceCutoffHigh=200):
+    res = []
+    removedLong = 0
+    removedShort = 0
+    for t in trees:
+        length = count_leaf_nodes(t)
+        high = length
+        if sentenceCutoffHigh != -1:
+            high = sentenceCutoffHigh
+        if length > sentenceCutoffLow and length <= high:
+            res.append(t)
+        elif length <= sentenceCutoffLow:
+            removedShort += 1
+        else:
+            removedLong += 1
+
+    print("cleanTreesByLength: removed short {}, removed long {}. Previous {} New length {}".format(removedShort, removedLong, len(trees), len(res)))
+    return res
+
+def cleanTreesByBadChars(trees):
+    res = []
+    ignoredSentenceCount = 0
+    for t in trees:
+        sentence = output_sentence(t)
+        for c in sentence:
+            if c < '\x1f':
+                if c != '\n' and c != '\r' and c != '\f':
+                    ignoredSentenceCount += 1
+                    continue  # ignore this sentence
+        res.append(t)
+    print("cleanTreesByBadChars Cleaned: {}".format(ignoredSentenceCount))
+    return res
+
+class SentenceCounter:
+    def __init__(self, trees, labelList):
+        # count number of occurences of each sentence (mostly relevant for repeating sentences)
+        self.labelList = labelList
+        self.labelMap = {}
+        for i, label in enumerate(labelList):
+            self.labelMap[label] = i
+        self.sentences = {}
+        for t in trees:
+            sentence = output_sentence(t)
+            if sentence not in self.sentences:
+                self.sentences[sentence] = [0 for label in labelList]
+            self.sentences[sentence][self.labelMap[t.syntax]] += 1
+        self.ambigousCount = 0
+        self.ignoredSentenceCount = 0
+        self.callCount = 0
+        print("SentenceCounter initialized")
+
+    def isSentenceAmbigous(self, sentence, sentenceLabel):
+        self.callCount += 1
+        countValues = 0
+        for labelIndex in range(len(self.labelList)):
+            if self.sentences[sentence][labelIndex] > 0:
+                countValues += 1
+        if countValues == 1:
+            # unambigous
+            return False
+        else:
+            # ambigous
+            self.ambigousCount += 1
+            maxLabelIndex = 0
+            for labelIndex in range(len(self.labelList)):
+                if self.sentences[sentence][labelIndex] > self.sentences[sentence][maxLabelIndex]:
+                    maxLabelIndex = labelIndex
+            if self.labelMap[sentenceLabel] == maxLabelIndex:
+                return False
+            self.ignoredSentenceCount += 1
+            return True
+
+    def cleanAmbigous(self, trees):
+        res = []
+        for t in trees:
+            sentence = output_sentence(t)
+            if not self.isSentenceAmbigous(sentence, t.syntax):
+                res.append(t)
+        self.reportFindings()
+        return res
+
+    def reportFindings(self):
+        print("contented {} ignored {}. New size {}".format(self.ambigousCount, self.ignoredSentenceCount, self.callCount - self.ignoredSentenceCount))
+
