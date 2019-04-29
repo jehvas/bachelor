39ec90ee922fdac369d12747011d31e4c68fef5f
Jens Hvass <jehj@itu.dk>
2019-04-29 12:32:16 +0200

    Enron classes

--- src/DatasetsConsumers/EnronEvidence.py
+++ src/DatasetsConsumers/EnronEvidence.py
@@ -5,7 +5,7 @@ DATA_PATH = "data/SensitiveEnron/evidence_tampering"
 
 class EnronEvidence(SensitiveEnron):
     def set_classes(self) -> None:
-        self.classes = [0, 1]
+        self.classes = ['Sensitive', 'Non-Sensitive']
 
     def common_load(self, data_path):
         return super().common_load(data_path)
