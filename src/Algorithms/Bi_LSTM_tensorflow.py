27d39a082d8d4bb9b17a9cf04e7a02f9ba2a176f
Jens Hvass <jehj@itu.dk>
2019-05-02 13:18:10 +0200

    static LSTM

--- src/Algorithms/Bi_LSTM_tensorflow.py
+++ src/Algorithms/Bi_LSTM_tensorflow.py
@@ -1,6 +1,6 @@
 from Algorithms.AbstractTensorflowAlgorithm import AbstractTensorflowAlgorithm
 from tensorflow.python.keras.engine.sequential import Sequential
-from tensorflow.python.keras.layers import Embedding, Dense, LSTM
+from tensorflow.python.keras.layers import Embedding, Dense, LSTM, CuDNNLSTM
 from utility.model_factory import make_hidden_layers
 
 
@@ -17,5 +17,5 @@ class Bi_LSTM_Tensorflow(AbstractTensorflowAlgorithm):
         self.model = Sequential(
             [Embedding(self.embedding.shape[0], self.embedding.shape[1], weights=[self.embedding],
                        input_length=self.input_dim, trainable=False),
-             LSTM(128),
+             CuDNNLSTM(128),
              Dense(20, activation='softmax')])
