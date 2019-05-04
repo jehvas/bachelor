1669cdd3590d7e8fa6e94c44e8e0e60adc3d397e
Mads <madspdrsen@gmail.com>
2019-05-04 15:03:34 +0200

    ikke activation_func på cudnnlstm

--- src/utility/model_factory.py
+++ src/utility/model_factory.py
@@ -29,7 +29,7 @@ def make_hidden_layers(middle_layers, input_shape):
             else:
                 print("Please install GPU version of TF")
                 if idx == 0:
-                    layers.append(Bidirectional(LSTM(size, activation=activation_func, input_shape=input_shape, return_sequences=True)))
+                    layers.append(Bidirectional(LSTM(size, input_shape=input_shape, return_sequences=True)))
                 else:
-                    layers.append(Bidirectional(LSTM(size, activation=activation_func)))
+                    layers.append(Bidirectional(LSTM(size)))
     return layers
