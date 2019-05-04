dd8159a3a10ce2ab9cbb154e0641434712816c05
Jens Hvass <jehj@itu.dk>
2019-05-05 01:53:11 +0200

    rootfile

--- src/rootfile.py
+++ src/rootfile.py
@@ -5,8 +5,8 @@ ROOTPATH = os.path.dirname(__file__) + '/../'
 if os.path.isdir(ROOTPATH + 'out/'):
     print('Running locally. Using standard ROOTPATH')
 else:
-    if ROOTPATH.find('src') != -1:
-        ROOTPATH = os.path.dirname(__file__) + '/../../../drive/My Drive/Bachelor/'
-    else:
-        ROOTPATH = os.path.dirname(__file__) + '/../../drive/My Drive/Bachelor/'
+    # if ROOTPATH.find('src') != -1:
+    #     ROOTPATH = os.path.dirname(__file__) + '/../../../drive/My Drive/Bachelor/'
+    # else:
+    ROOTPATH = os.path.dirname(__file__) + '/../../drive/My Drive/Bachelor/'
     print('Running on COLAB. Using alertnate ROOTPATH')
