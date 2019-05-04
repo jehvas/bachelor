23cc1a8474c89f60c5bdbdf4e807313dc5b8a9ec
Jens Hvass <jehj@itu.dk>
2019-05-04 22:27:00 +0200

    tmpfix

--- src/memsafe_seach.py
+++ src/memsafe_seach.py
@@ -7,4 +7,4 @@ datasets_to_use = sys.argv[2].lower()
 amount = int(sys.argv[3])
 
 while True:
-    os.system('python src/parameter_search.py {} {} {}'.format(algorithms_to_use, datasets_to_use, amount))
+    os.system('python parameter_search.py {} {} {}'.format(algorithms_to_use, datasets_to_use, amount))
--- src/rootfile.py
+++ src/rootfile.py
@@ -6,4 +6,6 @@ if os.path.isdir(ROOTPATH + 'out/'):
     print('Running locally. Using standard ROOTPATH')
 else:
     ROOTPATH = os.path.dirname(__file__) + '/../../drive/My Drive/Bachelor/'
+    if ROOTPATH.find('src') != -1:
+        ROOTPATH += '../'
     print('Running on COLAB. Using alertnate ROOTPATH')
