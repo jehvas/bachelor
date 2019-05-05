b23f82cdf77041d136b5fa5a43f01c3ae767f39c
Jens Hvass <jehj@itu.dk>
2019-05-05 12:25:36 +0200

    mmem

--- src/memsafe_search.py
+++ src/memsafe_search.py
@@ -8,7 +8,7 @@ import numpy as np
 algorithms_to_use = sys.argv[1].lower()
 datasets_to_use = sys.argv[2].lower()
 amount = int(sys.argv[3])
-
+mode = sys.argv[4]
 while True:
     # os.system('python test.py')
-    os.system('python parameter_search.py {} {} {}'.format(algorithms_to_use, datasets_to_use, amount))
+    os.system('python parameter_search.py {} {} {} {}'.format(algorithms_to_use, datasets_to_use, amount, mode))
